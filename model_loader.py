a2f4616e5694ee1ffa0f2e6a3d262438d13e9f56
rokgyu <rokgyu5@gmail.com>
2024-11-23 23:54:40 +0900

    Update model_loader.py

--- model_loader.py
+++ model_loader.py
@@ -1,8 +1,24 @@
-# model_loader.py
-
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from transformers import M2M100Tokenizer, M2M100ForConditionalGeneration
 
-def load_model(model_name):
-    tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-ko-en")
-    model = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-ko-en")
+def load_model_1():
+    """Helsinki-NLP의 Korean-English 번역 모델을 로드합니다."""
+    model_name = "Helsinki-NLP/opus-mt-ko-en"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+    return tokenizer, model
+
+def load_model_2():
+    """Facebook의 M2M100 다국어 번역 모델을 로드합니다."""
+    model_name = "facebook/m2m100_418M"
+    tokenizer = M2M100Tokenizer.from_pretrained(model_name)
+    model = M2M100ForConditionalGeneration.from_pretrained(model_name)
+    tokenizer.src_lang = "ko"
+    return tokenizer, model
+
+def load_model_3():
+    """NLLB 다국어 번역 모델을 로드합니다."""
+    model_name = "facebook/nllb-200-distilled-600M"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
     return tokenizer, model
